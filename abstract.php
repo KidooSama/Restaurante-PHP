5471e63cbd68a4243681085b368cf19443fd55b5
KidooSAma <tallesbraga63@gmail.com>
2023-04-01 17:48:18 -0300

    Create more files, Add more classes

---
+++ abstract.php
@@ -0,0 +1,45 @@
+<?php
+
+abstract class abs_rest{
+
+  public $name_rest;#string
+  public $type_rest;#string
+  protected $price_rest;#float
+  protected $vaga_rest;#int
+  public $addr_rest;#string
+  public $time_rest;#string
+
+  abstract public function setRestAtrs($name,$type,$price,$vahan,$addr,$time);
+  abstract public function getRestAtrs();
+}
+
+abstract class abs_food{
+  protected $name_food;#string
+  protected $desc_food;#string
+  protected $price_food;#float
+  protected $cat_food;#string
+  protected $rate_food;#float
+
+
+  abstract public function setFoodAtrs($name,$desc,$price,$cat,$rate);
+  abstract public function getFoodAtrs();
+}
+
+
+abstract class abs_receipt{
+  // protected $name_rest;#string
+  // protected $name_food;#string
+  // protected $price_food;#float
+  protected $id_receipt;
+  protected $pay_receipt;#string
+  protected $client_receipt;#float
+  protected $cpf_receipt;#float
+
+
+  abstract public function setReceiptAtrs($id,$pay,$cli,$cpf);
+  abstract public function getReceiptAtrs();
+
+}
+
+
+?>
---
+++ receipt.php
@@ -0,0 +1,23 @@
+<?php
+// require_once "abstract.php";
+//  class rest extends abs_rest{
+
+//     public function setRestAtrs($name,$type,$price,$vaga,$addr,$time) {
+//         $this->name_rest = $name;
+//         $this->type_rest = $type;
+//         $this->price_rest = $price;
+//         $this->vaga_rest = $vaga;
+//         $this->addr_rest = $addr;
+//         $this->time_rest = $time;
+//       }
+//       public function getRestAtrs(){
+//         return "\n".$this->name_rest."\n".$this->type_rest."\n".$this->price_rest."\n".$this->vaga_rest."\n".$this->time_rest."\n".$this->addr_rest; 
+//       }
+// }
+
+// // $as = new rest();
+// // $as->setRestAtrs("Bonnapart", "Restaurante","$$$$$",2,"Rua Cruzado, 86","18:00 - 23:00");
+// // echo $as->getRestAtrs();
+
+
+?>
